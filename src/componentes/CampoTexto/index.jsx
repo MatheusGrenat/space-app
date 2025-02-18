f02b43cdbea24c8e5a45c366ca89c77b0669c653
MatheusGrenat <matheusgrenat@yahoo.com.br>
2025-02-18 19:21:35 -0300

    estilizando o componente CampoTexto

--- src/componentes/CampoTexto/index.jsx
+++ src/componentes/CampoTexto/index.jsx
@@ -1,10 +1,15 @@
 import styles from './CampoTexto.module.css';
+import lupa from '/imagens/lupa.png';
+
 
 
 
 export default function CampoTexto() {
     return(
-        <input type="text" placeholder="O que você procura..." className={styles.campoTexto}/>
+        <>
+            <input type="text" placeholder={`O que você procura?`} className={styles.campoTexto}/> 
+            
+        </>
     )
 }
 
