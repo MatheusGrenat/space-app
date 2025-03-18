8c42520b8851a578f76189530414b43d02b28b2c
MatheusGrenat <matheusgrenat@yahoo.com.br>
2025-03-18 19:08:56 -0300

    Importando as imagens e alterando o componente galeria

--- src/componentes/Galeria/Imagem/index.jsx
+++ src/componentes/Galeria/Imagem/index.jsx
@@ -1,11 +1,14 @@
+import fotos from "../../../fotos.json"
+
 const Imagem = ({ foto }) => {
     return(
         <figure>
             <img></img>
             <figcaption>
-                <h3></h3>
                 <footer>
                     <p></p>
+                    <h3>{fotos.map(foto => foto.titulo)}</h3>
+                    <h4>{fotos.map(foto => foto.fonte)}</h4>
                     <button>Favorito</button>
                     <button>Expandir</button>
                 </footer>
